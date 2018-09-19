83c6501acb481be46936a1f0bc6f4cb16f3dd1d7
Plumblumpb <2573350997@qq.com>
2018-09-19 14:53:13 +0800

    多线程测试。

--- src/A3等待和唤醒/WaitNotify1.java
+++ src/A3等待和唤醒/WaitNotify1.java
@@ -9,29 +9,25 @@ public class WaitNotify1 {
 
 
     public static void main(String[] args) {
+        WaitNotify1 waitNotify1 = new WaitNotify1();
+        Run1 run = new Run1();
 
-        final Object object = new Object();
+        for(int j = 0; j< 2; j++){
+            Thread thread = new Thread(run);
+            thread.start();
 
-        new Thread(new Runnable() {
+        }
 
-            public void run() {
+    }
 
-                synchronized (object) {
-                    System.out.println("线程1 获取到监视器锁");
-                    try {
-                        object.wait();
-                        System.out.println("线程1 恢复啦。我为什么这么久才恢复，因为notify方法虽然早就发生了，可是我还要获取锁才能继续执行。");
-                    } catch (InterruptedException e) {
-                        System.out.println("线程1 wait方法抛出了InterruptedException异常");
-                    }
-                }
-            }
-        }, "线程1").start();
+    static final Object object = new Object();
+    static int i = 1;
+    static class Run1 implements Runnable{
+        @Override
+        public void run() {
+            synchronized (object) {
 
-        new Thread(new Runnable() {
-
-            public void run() {
-                synchronized (object) {
+                if(i == 0){
                     System.out.println("线程2 拿到了监视器锁。为什么呢，因为线程1 在 wait 方法的时候会自动释放锁");
                     System.out.println("线程2 执行 notify 操作");
                     object.notify();
@@ -43,10 +39,18 @@ public class WaitNotify1 {
 
                     }
                     System.out.println("线程2 休息够了，结束操作");
+                }else {
+                    System.out.println("线程1 获取到监视器锁");
+                    try {
+                        i = 0;
+                        object.wait();
+                        System.out.println("线程1 恢复啦。我为什么这么久才恢复，因为notify方法虽然早就发生了，可是我还要获取锁才能继续执行。");
+                    } catch (InterruptedException e) {
+                        System.out.println("线程1 wait方法抛出了InterruptedException异常");
+                    }
                 }
             }
-        }, "线程2").start();
+
+        }
     }
-
-
 }
